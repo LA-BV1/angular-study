f6e095c97f43f53d0785fae4ffd58b44d9b91e68
Volodymyr-Bairamov <likazorjak123@gmail.com>
2018-04-05 20:51:29 +0300

    Init angular study and create base architecture.

---
+++ src/app/Login/Login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './Login.component.html',
+})
+export class LoginComponent {
+  title = 'It is login component!!!';
+}
