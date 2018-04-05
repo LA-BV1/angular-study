b684b34def73cb9493f1ecf6157138301692120f
Volodymyr-Bairamov <likazorjak123@gmail.com>
2018-04-06 00:12:53 +0300

    Created form auth and connected to bootstrap.

--- src/app/Login/Login.component.ts
+++ src/app/Login/Login.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './Login.component.html',
+  styleUrls: ['./Login.component.css']
 })
 export class LoginComponent {
   title = 'It is login component!!!';
